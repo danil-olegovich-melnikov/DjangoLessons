00c2564f0b1b562ede2b90808eb91b637bb49040
user <danil369.ru@mail.ru>
2022-02-14 10:17:33 +0600

    PUT + composition

---
+++ car/utils.py
@@ -0,0 +1,39 @@
+from .db import *
+
+
+if 'cars' not in globals():
+    cars = []
+
+def put(pk: int, name: str, speed: int, price: int) -> None:
+    """ Replace the car instance by pk """
+
+    car_index = -1
+
+    for index, car in enumerate(cars):
+        if car.id == pk:
+            car_index = index
+            break
+
+    if car_index == -1:
+        return
+
+    cars[car_index] = Car(pk, name, speed, price)
+
+def delete(pk: int) -> None:
+    """ Delete the car instance by pk """
+
+    global cars
+    cars = [car for car in cars if car.id != pk]
+
+
+def append(name: str, speed: int, price: int) -> None:
+    """ Append the car instance """
+    cars.append(
+        Car(
+            id=cars[-1].id + 1,
+            name=name,
+            speed=speed,
+            price=price,
+        )
+    )
+
--- car/views.py
+++ car/views.py
@@ -1,46 +1,33 @@
-from collections import namedtuple
-from random import randint
-from typing import List
-
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.urls import reverse
-
-Car = namedtuple('Car', ['id', 'name', 'price', 'speed'])
-
-cars: List[Car] = [
-    Car(1, 'Audi', randint(1, 100) * 100000, 300),
-    Car(2, 'BWM', randint(1, 100) * 100000, 320),
-    Car(3, 'Mercedes', randint(1, 100) * 100000, 340),
-    Car(4, 'Lada', randint(1, 100), 30),
-]
-
-
-def delete(pk):
-    global cars
-    cars = [car for car in cars if car.id != pk]
+from . import utils
 
 
 # Create your views here.
 def car_list(request: HttpRequest) -> HttpResponse:
-    return render(request, 'car/index.html', context={"cars": cars})
+    return render(request, 'car/index.html', context={"cars": utils.cars})
 
 
-def car_delete(request: HttpRequest, pk) -> HttpResponse:
-    delete(pk)
+def car_delete(request: HttpRequest, pk: int) -> HttpResponseRedirect:
+    utils.delete(pk)
     return redirect(reverse('car:car_list'))
 
 
-def car_add(request: HttpRequest) -> HttpResponse:
+def car_add(request: HttpRequest) -> HttpResponseRedirect:
     if request.method == "POST":
-        cars.append(
-            Car(
-                id=cars[-1].id + 1,
-                name=request.POST.get("name", ""),
-                speed=request.POST.get("speed", ""),
-                price=request.POST.get("price", ""),
+        utils.append(request.POST.get("name", ""), request.POST.get("speed", ""), request.POST.get("price", ""))
+    return redirect(reverse('car:car_list'))
+
+
+def car_put(request: HttpRequest) -> HttpResponseRedirect:
+    if request.method == "POST":
+        utils.put(
+            int(request.POST.get("pk", -1)),
+            request.POST.get("name", ""),
+            int(request.POST.get("speed", -1)),
+            int(request.POST.get("price", -1))
             )
-        )
     return redirect(reverse('car:car_list'))
